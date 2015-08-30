34f4c78015345db6f78c995e56860b863795eec2
farisj <jakefaris@outlook.com>
2015-08-30 15:21:28 -0400

    stubs out script for periodic tweets

--- bot.rb
+++ bot.rb
@@ -1,9 +1,22 @@
 require 'rubygems'
 require 'twitter'
+require './twitter_keys'
+require 'open-uri'
+require 'pry'
+
+adjectives = open("https://gist.githubusercontent.com/farisj/cc70300356eca8f54c47/raw/9885ef80b27c3834d654a158fe8d90321725c154/adjectives.txt").read.split("\n")
+
 
 client = Twitter::REST::Client.new do |config|
-  # config.consumer_key = 
-  # config.consumer_secret = 
-  # config.access_token = 
-  # config.access_token_secret = 
+  config.consumer_key = TWITTER_KEYS[:consumer_key]
+  config.consumer_secret = TWITTER_KEYS[:consumer_secret]
+  config.access_token = TWITTER_KEYS[:access_token]
+  config.access_token_secret = TWITTER_KEYS[:access_token_secret]
+end
+
+
+adjectives.each do |adjective|
+
+  # client.update("#{adjective} orb")
+
 end
