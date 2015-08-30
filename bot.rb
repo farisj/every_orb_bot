c5fef8c7bb6679bccd1b8aa28b03f72c328e2fb2
farisj <jakefaris@outlook.com>
2015-08-30 18:41:32 -0400

    changes symbol to string for env variables

--- bot.rb
+++ bot.rb
@@ -6,10 +6,10 @@ adjective_url = "https://gist.githubusercontent.com/farisj/cc70300356eca8f54c47/
 adjectives = open(adjective_url).read.split("\n")
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key = ENV[:consumer_key]
-  config.consumer_secret = ENV[:consumer_secret]
-  config.access_token = ENV[:access_token]
-  config.access_token_secret = ENV[:access_token_secret]
+  config.consumer_key = ENV["consumer_key"]
+  config.consumer_secret = ENV["consumer_secret"]
+  config.access_token = ENV["access_token"]
+  config.access_token_secret = ENV["access_token_secret"]
 end
 
 most_recent_tweet = client.user_timeline("everyorb").first
