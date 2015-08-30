bb1f646be0597c417efa29b7e0a34a1e2175fbe6
farisj <jakefaris@outlook.com>
2015-08-30 18:38:00 -0400

    moves keys to a figaro application.yml

--- bot.rb
+++ bot.rb
@@ -1,5 +1,4 @@
 require 'twitter'
-require './twitter_keys'
 require 'open-uri'
 require 'pry'
 
@@ -8,10 +7,10 @@ adjective_url = "https://gist.githubusercontent.com/farisj/cc70300356eca8f54c47/
 adjectives = open(adjective_url).read.split("\n")
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key = TWITTER_KEYS[:consumer_key]
-  config.consumer_secret = TWITTER_KEYS[:consumer_secret]
-  config.access_token = TWITTER_KEYS[:access_token]
-  config.access_token_secret = TWITTER_KEYS[:access_token_secret]
+  config.consumer_key = ENV[:consumer_key]
+  config.consumer_secret = ENV[:consumer_secret]
+  config.access_token = ENV[:access_token]
+  config.access_token_secret = ENV[:access_token_secret]
 end
 
 most_recent_tweet = client.user_timeline("everyorb").first
