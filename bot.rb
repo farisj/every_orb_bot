325def5ddf2e610aa04596b05c2f33aa26ca9ee8
farisj <jakefaris@outlook.com>
2015-08-30 17:29:03 -0400

    creates task to iterate through and tweet

--- bot.rb
+++ bot.rb
@@ -4,8 +4,9 @@ require './twitter_keys'
 require 'open-uri'
 require 'pry'
 
-adjectives = open("https://gist.githubusercontent.com/farisj/cc70300356eca8f54c47/raw/9885ef80b27c3834d654a158fe8d90321725c154/adjectives.txt").read.split("\n")
+adjective_url = "https://gist.githubusercontent.com/farisj/cc70300356eca8f54c47/raw/9885ef80b27c3834d654a158fe8d90321725c154/adjectives.txt"
 
+adjectives = open(adjective_url).read.split("\n")
 
 client = Twitter::REST::Client.new do |config|
   config.consumer_key = TWITTER_KEYS[:consumer_key]
@@ -14,9 +15,14 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = TWITTER_KEYS[:access_token_secret]
 end
 
+most_recent_tweet = client.user_timeline("everyorb").first
 
-adjectives.each do |adjective|
+if most_recent_tweet
 
-  # client.update("#{adjective} orb")
+  most_recent_adjective = most_recent_tweet.text.split(" ").first
+
+  recent_adj_index = adjectives.find_index(most_recent_adjective)
+
+  client.update("#{adjectives[recent_adj_index+1]} orb")
 
 end
